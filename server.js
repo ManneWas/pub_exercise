a5d51a0254ce4ad74615bbe92607ee94e39345a5
krubear <torkild.kruber@gmail.com>
2022-03-04 15:08:20 +0100

    Torkild Commit

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ server.use( express.json() )
 
 // start server
 server.listen(3000, () =>{
-    console.log( 'Server is running at localhost:3000/data')
+    console.log( 'Server is running at http://localhost:3000/data')
 })
 
 const sqlite3 = require ('sqlite3')
-
+const util = require('util')
 const db = new sqlite3.Database('./database/pub.db')
 db.all = util.promisify(db.all)
 db.run = util.promisify(db.run)
 
+server.get('/data/alcohol', async(request, response)=>{
+    let result = await db.all("SELECT * FROM alcohol")
+
+    response.json(result)
+})
