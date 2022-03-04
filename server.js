9b851cc1e98fd5aaf64804d1aacc69cde9f02755
Dannebanne94 <daniel_jonsson@hotmail.se>
2022-03-04 15:54:25 +0100

    Daniel_Sanjin

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express')
 const server = express()
-server.use(express.json())  //request json body
+server.use(express.json())  
 
-//start server
 server.listen(3000, ()=>{
     console.log("server running at http://localhost:3000/data")
 })
 
 const sqlite3 = require('sqlite3')
 
-//"promisify" = create promises for methods that are using callbacks
-//why? because then we can use async/await syntax
 const util = require('util')
 
 const db = new sqlite3.Database('./database/pub.db')
@@ -20,6 +17,41 @@ db.run = util.promisify(db.run)
 //get list of alcohol
 server.get('/data/alcohol', async(request, response)=>{
     let result = await db.all('SELECT * FROM alcohol')
+
+    response.json(result)
+})
+
+//get record of alcohol
+server.get('/data/alcohol/:item_id', async(request, response)=>{
+    let result = await db.all('SELECT * FROM alcohol WHERE item_id = ?', [request.params.item_id])
+
+    response.json(result)
+})
+
+//get list of all inventory
+server.get('/data/inventory', async(request, response)=>{
+    let result = await db.all('SELECT * FROM inventory')
+
+    response.json(result)
+})
+
+//get record of inventory
+server.get('/data/inventory/:item_id', async(request, response)=>{
+    let result = await db.all('SELECT * FROM alcohol WHERE item_id = ?', [request.params.item_id])
+
+    response.json(result)
+})
+
+//get list of all drinks
+server.get('/data/drink_menu', async(request, response)=>{
+    let result = await db.all('SELECT * FROM drink_menu')
+
+    response.json(result)
+})
+
+//get record of drink
+server.get('/data/drink_menu/:drink_id', async(request, response)=>{
+    let result = await db.all('SELECT * FROM drink_menu WHERE drink_id = ?', [request.params.drink_id])
 
     response.json(result)
 })
